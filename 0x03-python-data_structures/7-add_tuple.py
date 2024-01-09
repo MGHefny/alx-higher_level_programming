b2fd937f798fa9f75910b87c9955ade664236a25
MGHefny <mgenedhefny@gmail.com>
2024-01-09 04:00:26 +0200

    m7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     la, lb = len(tuple_a), len(tuple_b)
     nt = ((tuple_a[0] if la >= 1 else 0) +
-                 (tuple_b[0] if lb >= 1 else 0),
-                 (tuple_a[1] if la >= 2 else 0) +
-                 (tuple_b[1] if lb >= 2 else 0))
+          (tuple_b[0] if lb >= 1 else 0),
+          (tuple_a[1] if la >= 2 else 0) +
+          (tuple_b[1] if lb >= 2 else 0))
     return nt
