9093efb1aaafc44a5dcd0f0b3dcd3d96854deb72
MGHefny <mgenedhefny@gmail.com>
2024-01-24 04:29:04 +0200

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Represent square"""
+    def __init__(self, size):
+        """Initialize a new Square"""
+        self.__size = size
