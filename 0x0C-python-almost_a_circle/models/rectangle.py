6cb4e07125fcce34e9874a39e6e25828e11d8d17
MGHefny <mgenedhefny@gmail.com>
2024-02-13 01:40:16 +0200

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Rectangle class"""
+'''Module for Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
+    '''A Rectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        '''Constructor.'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,6 +16,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''Width of this rectangle.'''
         return self.__width
 
     @width.setter
@@ -24,6 +26,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        '''Height of this rectangle.'''
         return self.__height
 
     @height.setter
@@ -33,6 +36,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        '''x of this rectangle.'''
         return self.__x
 
     @x.setter
@@ -42,6 +46,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        '''y of this rectangle.'''
         return self.__y
 
     @y.setter
@@ -50,6 +55,7 @@ class Rectangle(Base):
         self.__y = value
 
     def validate_integer(self, name, value, eq=True):
+        '''Method for validating the value.'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
@@ -58,19 +64,23 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
 
     def area(self):
+        '''Computes area of this rectangle.'''
         return self.width * self.height
 
     def display(self):
-        z = '\n' * self.y + \
+        '''Prints string representation of this rectangle.'''
+        s = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
-        print(z, end='')
+        print(s, end='')
 
     def __str__(self):
+        '''Returns string info about this rectangle.'''
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
 
     def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
         if id is not None:
             self.id = id
         if width is not None:
@@ -82,13 +92,15 @@ class Rectangle(Base):
         if y is not None:
             self.y = y
 
-    def update(self, *arg, **narg):
-        # print(arg, narg)
-        if arg:
-            self.__update(*arg)
-        elif narg:
-            self.__update(**narg)
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        # print(args, kwargs)
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
     def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
         return {"id": self.id, "width": self.__width, "height": self.__height,
                 "x": self.__x, "y": self.__y}
