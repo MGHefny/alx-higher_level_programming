e4e2371850a9987963863ffe8b10b63072157a46
MGHefny <mgenedhefny@gmail.com>
2024-04-10 09:00:19 +0200

    3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let n = 0; n < this.height; n++) {
+      let y = '';
+      for (let z = 0; z < this.width; z++) {
+        y += 'X';
+      }
+      console.log(y);
+    }
+  }
+}
+module.exports = Rectangle;
