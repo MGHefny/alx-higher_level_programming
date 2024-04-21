25240807aa0ef741a649d85fe8f3c33011c670f0
MGHefny <mgenedhefny@gmail.com>
2024-04-22 00:23:07 +0200

    1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                                 passwd=argv[2], db=argv[3], charset="utf8")
+    current = connection.cursor()
+    current.execute("SELECT * FROM states ORDER BY states.id ASC")
+    qrow = current.fetchall()
+    for r in qrow:
+        if r[1].startswith("N"):
+            print(r)
+    current.close()
+    connection.close()
