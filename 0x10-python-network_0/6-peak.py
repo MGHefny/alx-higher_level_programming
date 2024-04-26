61e77adeede71f5a88921c6499d1b95478f048d6
MGHefny <mgenedhefny@gmail.com>
2024-04-26 05:21:41 +0300

    f

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """peak"""
 
-
 def find_peak(list_of_integers):
     """peak"""
     if not list_of_integers:
         return None
-    else:
-        return max(list_of_integers)
+
+    r = len(list_of_integers) - 1
+    l = 0
+
+    while l < r:
+        middle = (l + r) // 2
+        if list_of_integers[middle] < list_of_integers[middle + 1]:
+            l = middle + 1
+        else:
+            r = middle
+
+    return list_of_integers[l]
+
