6f4d0add5276cc3d2892f1a63433c86d49de3a49
MGHefny <mgenedhefny@gmail.com>
2024-04-26 04:22:23 +0300

    6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""peak"""
+
+
+def find_peak(list_of_integers):
+    """peak"""
+    if not len(list_of_integers):
+        return None
+    else:
+        return max(list_of_integers)
