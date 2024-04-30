e04a11c310db51e8e411a0c40f01bf0e74e79009
MGHefny <mgenedhefny@gmail.com>
2024-05-01 00:04:01 +0300

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ sends request to URL and display body """
 import sys
-import urllib.request
+from urllib import request, error
 
 
 if __name__ == '__main__':
     argv = sys.argv
     u = argv[1]
     try:
-        with request.urlopen(u) as x:
-            print(x.read().decode('utf-8'))
-    except error.HTTPError as err:
-        print("Error code: {}".format(err.status))
+        with request.urlopen(u) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as errorr:
+        print("Error code: {}".format(errorr.status))
