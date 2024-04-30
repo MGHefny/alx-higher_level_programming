f1826181fe536926fc9cba07b4bf3ad6a74b2a77
MGHefny <mgenedhefny@gmail.com>
2024-05-01 01:06:56 +0300

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,6 +6,6 @@ from requests import get
 if __name__ == "__main__":
     u = argv[1]
     x = get(u)
-    ERR_TXT = 'Error code: {}'
+    errorr = 'Error code: {}'
     status = x.status_code
-    print(ERR_TXT.format(status) if (status >= 400) else x.text)
+    print(errorr.format(status) if (status >= 400) else x.text)
