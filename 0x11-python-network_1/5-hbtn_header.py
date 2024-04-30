afe082ee031aa30dfff93156d79fa48c526f11bf
MGHefny <mgenedhefny@gmail.com>
2024-05-01 00:16:04 +0300

    5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" fetch X-Request-Id """
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    print(req.headers.get('X-Request-Id'))
