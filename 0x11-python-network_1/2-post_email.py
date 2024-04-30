dedadc9c3bf4b31dde66bbf42fdaa3172b634e45
MGHefny <mgenedhefny@gmail.com>
2024-04-30 23:45:35 +0300

    2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" sends a POST request to URL"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == '__main__':
+    u = argv[1]
+    email = argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
+
+    with urllib.request.urlopen(u, DATA) as x:
+        print(x.read().decode('utf-8'))
