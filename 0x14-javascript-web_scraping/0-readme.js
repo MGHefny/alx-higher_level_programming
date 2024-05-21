2a9d5a4d426fc7bcd314b969527f91001b2f7797
MGHefny <mgenedhefny@gmail.com>
2024-05-22 01:10:51 +0300

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 
 const callsys = require('fs');
 callsys.readFile(process.argv[2], 'utf-8',
-  function (error_object, data) {
-    if (error_object) {
-      console.log(error_object);
+  function (errorobject, data) {
+    if (errorobject) {
+      console.log(errorobject);
       return;
     }
     console.log(data);
