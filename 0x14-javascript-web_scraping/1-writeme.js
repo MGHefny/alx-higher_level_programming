377fbbcca408950bc6e25252c02143e86cad9a6a
MGHefny <mgenedhefny@gmail.com>
2024-05-22 01:14:51 +0300

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const callsys = require('callsys');
+const callsys = require('fs');
 callsys.writeFile(process.argv[2], process.argv[3], 'utf-8',
   function (errorobject) {
     if (errorobject) {
