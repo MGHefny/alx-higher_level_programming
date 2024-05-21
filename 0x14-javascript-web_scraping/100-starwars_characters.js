d07bf41c14f104df3a9e215801dd2115fca47df2
MGHefny <mgenedhefny@gmail.com>
2024-05-22 01:57:08 +0300

    adv1

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,13 +4,15 @@ const url = 'https://swapi-api.alx-tools.com/api/films/';
 const idd = process.argv[2];
 reqq.get(url + idd, (errorobject, respo, thebody) => {
   if (!errorobject) {
-    const chars = JSON.parse(thebody).chars;
-    for (const charr of chars) {
-      reqq.get(charr, (errorobject, respo, thebody) => {
+    const cnt = JSON.parse(thebody);
+    const chars = cnt.chars;
+    for (const chaar of chars) {
+      reqq.get(chaar, (errorobject, respo, thebody) => {
         if (errorobject) {
           console.log(errorobject);
         } else {
-          console.log(JSON.parse(thebody).name);
+          const nms = JSON.parse(thebody);
+          console.log(nms.name);
         }
       });
     }
